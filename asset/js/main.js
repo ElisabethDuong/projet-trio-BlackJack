2aa68499cc227c0f0935dcb2552e36d1fc50646c
ElisabethDuong <elisabeth.duong@gmail.com>
2021-01-19 14:14:32 +0100

    ajout fonction draw

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,13 +1,16 @@
-var joueur = 0;
-var min = 16;
-var max = 21;
-var banque = Math.floor(Math.random() * (max - min) - min);
+$(document).ready(function () {
+    var joueur = 0;
+    var min = 16;
+    var max = 21;
+    var banque = Math.floor(Math.random() * (max - min) - min);
 
-function draw() {
-    min = 1;
-    max = 10;
-    joueur = joueur + (Math.floor(Math.random() * (max - min)) + min);
-    console.log(joueur)
-}
-
-draw();
+    function draw() {
+        min = 1;
+        max = 10;
+        joueur = joueur + (Math.floor(Math.random() * (max - min)) + min);
+        console.log(joueur)
+    }
+    $("#carte").click(function(){
+        $("#joueur").html(joueur);
+    });
+})
