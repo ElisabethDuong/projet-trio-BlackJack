b07b15b8c6198554039cf534c5617e25b9921d1b
SNarboni <sebastien.narboni@hotmail.fr>
2021-01-19 11:28:44 +0100

    début du js

--- asset/js/main.js
+++ asset/js/main.js
@@ -3,7 +3,12 @@ var min =16;
 var max= 21;
 var banque = Math.floor(Math.random() * (max - min) - min)
 
-
 function draw(){
-    
+    min = 1;
+    max = 10;
+    joueur= Math.floor(Math.random() * (max - min)) + min;
+    var somme = joueur;
+    console.log(joueur)
 }
+
+draw()
